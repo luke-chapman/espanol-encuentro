4c0b5eb0c90cbf8baa971e7a484038351b58dbc9
Luke Chapman <lrrchapman@gmail.com>
2024-07-30 17:03:36 +0200

    Write serialization and deserialization methods

--- src/espanol_encuentro/entry.py
+++ src/espanol_encuentro/entry.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+
+import yaml
 from pydantic import BaseModel, Field
 from typing import Literal
 
@@ -11,3 +14,16 @@ class Entry(BaseModel):
     longer_definition: list[str] = Field(default_factory=list)
     examples: list[str] = Field(default_factory=list)
     related_words: list[str] = Field(default_factory=list)
+
+
+def read_yaml_entries(filename: Path) -> list[Entry]:
+    with filename.open() as f:
+        contents = yaml.safe_load(f)
+    return [Entry.model_validate(c) for c in contents]
+
+
+def write_yaml_entries(entries: list[Entry], filename: Path) -> None:
+    contents = [e.model_dump(exclude_defaults=True) for e in entries]
+    filename.parent.mkdir(parents=True, exist_ok=True)
+    with filename.open("w") as f:
+        yaml.safe_dump(contents, f)
