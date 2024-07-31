7854a2f16103e9816a80b22f243885efe7901fd9
Luke Chapman <lrrchapman@gmail.com>
2024-07-31 10:32:13 +0200

    Tweaks to entry.py

--- src/espanol_encuentro/entry.py
+++ src/espanol_encuentro/entry.py
@@ -2,9 +2,13 @@ from pathlib import Path
 
 import yaml
 from pydantic import BaseModel, Field
-from typing import Literal
+from typing import Any, Literal
 
 PartOfSpeech = Literal["noun_f", "noun_m", "verb", "adjective", ""]
+
+
+def _yaml_dump(item: Any) -> str:
+    return yaml.safe_dump(item, sort_keys=False, allow_unicode=True)
 
 
 class Entry(BaseModel):
@@ -16,22 +20,22 @@ class Entry(BaseModel):
     related_words: list[str] = Field(default_factory=list)
 
     def __str__(self) -> str:
-        return yaml.safe_dump(self.model_dump(exclude_defaults=True), sort_keys=False)
+        return _yaml_dump(self.model_dump(exclude_defaults=True))
 
 
 def read_yaml_entries(filename: Path) -> list[Entry]:
     with filename.open() as f:
-        contents = yaml.safe_load(f)
-    entries = [Entry.model_validate(c) for c in contents]
+        model = yaml.safe_load(f)
+    entries = [Entry.model_validate(m) for m in model]
     print(f"Read {len(entries)} entries from {filename}")
     return entries
 
 
 def write_yaml_entries(entries: list[Entry], filename: Path) -> None:
-    contents = [e.model_dump(exclude_defaults=True) for e in entries]
+    model = [e.model_dump(exclude_defaults=True) for e in entries]
+    yaml_contents = _yaml_dump(model)
     filename.parent.mkdir(parents=True, exist_ok=True)
-    with filename.open("w") as f:
-        yaml.safe_dump(contents, f, sort_keys=False)
+    filename.write_text(yaml_contents)
     print(f"Wrote {len(entries)} to {filename}")
 
 
