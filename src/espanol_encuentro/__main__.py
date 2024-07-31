85ded45d93f6b51f76c26be964643143bf50079c
Luke Chapman <lrrchapman@gmail.com>
2024-07-31 08:30:18 +0200

    Add initial words to dictionary

--- src/espanol_encuentro/__main__.py
+++ src/espanol_encuentro/__main__.py
@@ -1,9 +1,52 @@
-# SPDX-FileCopyrightText: 2024-present U.N. Owen <void@some.where>
-#
-# SPDX-License-Identifier: MIT
 import sys
+from argparse import ArgumentParser
+from pathlib import Path
+from typing import get_args
+
+from espanol_encuentro.entry import PartOfSpeech, default_filename, read_yaml_entries, Entry, write_yaml_entries
+
+
+def main() -> None:
+    parser = ArgumentParser()
+    parser.add_argument("--filename", type=Path, help="Override to the location of the yaml lookup file")
+
+    subparsers = parser.add_subparsers(dest="mode")
+
+    lookup_parser = subparsers.add_parser("lookup")
+    lookup_parser.add_argument("word", help="The Spanish word to lookup")
+
+    add_parser = subparsers.add_parser("add")
+    add_parser.add_argument("word", help="The Spanish word to add")
+    add_parser.add_argument("--part-of-speech", "-p", choices=get_args(PartOfSpeech))
+    add_parser.add_argument("--short-definition", "-s")
+    add_parser.add_argument("--long-definition", "-l", nargs="*")
+    add_parser.add_argument("--examples", "-e", nargs="*")
+    add_parser.add_argument("--related-words", "-r", nargs="*")
+
+    args = parser.parse_args()
+    if args.mode == "lookup":
+        pass
+    elif args.mode == "add":
+        filename = args.filename or default_filename()
+        print(f"Will write file to {filename}")
+        entries = read_yaml_entries(filename) if filename.exists() else []
+
+        entry = Entry(
+            word=args.word,
+            part_of_speech=args.part_of_speech or "",
+            short_definition=args.short_definition or "",
+            long_definition=args.long_definition or [],
+            examples=args.examples or [],
+            related_words=args.related_words or []
+        )
+        entries.append(entry)
+        entries = sorted(entries, key=lambda e: (e.word, e.part_of_speech, e.short_definition))
+
+        write_yaml_entries(entries=entries, filename=filename)
+        print(f"Successfully added word {entry.word} to the lookup file")
+    else:
+        raise ValueError(f"Invalid mode {args.mode}")
+
 
 if __name__ == "__main__":
-    from espanol_encuentro.cli import espanol_encuentro
-
-    sys.exit(espanol_encuentro())
+    sys.exit(main())
--- src/espanol_encuentro/entry.py
+++ src/espanol_encuentro/entry.py
@@ -4,14 +4,14 @@ import yaml
 from pydantic import BaseModel, Field
 from typing import Literal
 
-PartOfSpeech = Literal["noun_f", "noun_m", "verb", "adjective"]
+PartOfSpeech = Literal["noun_f", "noun_m", "verb", "adjective", ""]
 
 
 class Entry(BaseModel):
     word: str
-    simple_definition: str
-    part_of_speech: PartOfSpeech
-    longer_definition: list[str] = Field(default_factory=list)
+    part_of_speech: PartOfSpeech = ""
+    short_definition: str = ""
+    long_definition: list[str] = Field(default_factory=list)
     examples: list[str] = Field(default_factory=list)
     related_words: list[str] = Field(default_factory=list)
 
@@ -19,11 +19,18 @@ class Entry(BaseModel):
 def read_yaml_entries(filename: Path) -> list[Entry]:
     with filename.open() as f:
         contents = yaml.safe_load(f)
-    return [Entry.model_validate(c) for c in contents]
+    entries = [Entry.model_validate(c) for c in contents]
+    print(f"Read {len(entries)} entries from {filename}")
+    return entries
 
 
 def write_yaml_entries(entries: list[Entry], filename: Path) -> None:
     contents = [e.model_dump(exclude_defaults=True) for e in entries]
     filename.parent.mkdir(parents=True, exist_ok=True)
     with filename.open("w") as f:
-        yaml.safe_dump(contents, f)
+        yaml.safe_dump(contents, f, sort_keys=False)
+    print(f"Wrote {len(entries)} to {filename}")
+
+
+def default_filename() -> Path:
+    return Path(__file__).resolve().parent.parent.parent / "data" / "words.yaml"
