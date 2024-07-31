42a5a43594bc1545336b204d6f2250a84047d332
Luke Chapman <lrrchapman@gmail.com>
2024-07-31 09:58:49 +0200

    Make lookup the default mode

--- src/espanol_encuentro/__main__.py
+++ src/espanol_encuentro/__main__.py
@@ -9,14 +9,10 @@ from espanol_encuentro.entry import PartOfSpeech, default_filename, read_yaml_en
 def main() -> None:
     parser = ArgumentParser()
     parser.add_argument("--filename", type=Path, help="Override to the location of the yaml lookup file")
+    parser.add_argument("word", help="The Spanish word to lookup")
 
     subparsers = parser.add_subparsers(dest="mode")
-
-    lookup_parser = subparsers.add_parser("lookup")
-    lookup_parser.add_argument("word", help="The Spanish word to lookup")
-
     add_parser = subparsers.add_parser("add")
-    add_parser.add_argument("word", help="The Spanish word to add")
     add_parser.add_argument("--part-of-speech", "-p", choices=get_args(PartOfSpeech))
     add_parser.add_argument("--short-definition", "-s")
     add_parser.add_argument("--long-definition", "-l", nargs="*")
@@ -28,7 +24,7 @@ def main() -> None:
     filename = args.filename or default_filename()
     entries = read_yaml_entries(filename) if filename.exists() else []
 
-    if args.mode == "lookup":
+    if not args.mode:
         words = [e for e in entries if e.word.lower() == args.word.lower()]
         print(f"Found {len(words)} entries for '{args.word}'")
         for w in words:
